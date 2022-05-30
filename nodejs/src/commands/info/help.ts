c7f7a9e393dcf3e2e688d7fbcb3114366a405edc
Severin Sprenger <sevisprengger@gmail.com>
2022-05-30 18:12:20 +0200

    Update help command for ontime command

--- nodejs/src/commands/info/help.ts
+++ nodejs/src/commands/info/help.ts
@@ -10,8 +10,9 @@ const message =
 '  map     Shows the current map rotation\n' +
 '  pingall Pings all user in a discord\n' +
 '  rank    Shows ranks of a player\n' +
-'  stats   Shows stats of a player\n' +
-'  status  Shows the status of all servers\n```')
+'  stats   Shows stats of a player (If no specific platform is provided, will default to PC)\n' +
+'  status  Shows the status of all servers\n' +
+'  ontime  Shows a leaderboard for the users that have spent the most time in a voice chat\n```')
 
 export default {
     callback: (ctx: Message, APEX_TOKEN: string, ...args: string[]) => {
