ba107e23703c81e47689becf56bd16cb4373a8ed
Severin Sprenger <sevisprengger@gmail.com>
2022-05-31 18:30:21 +0200

    Updated tested comment

--- nodejs/src/commands/apex/status.ts
+++ nodejs/src/commands/apex/status.ts
@@ -1,6 +1,6 @@
 import { Message, MessageEmbed, ReplyMessageOptions } from 'discord.js'
 
-//* Tested BakxY 21.05.2022 on version 1.14
+//* Tested BakxY 31.05.2022 on version 1.19
 
 export default {
     callback: async (ctx: Message, APEX_TOKEN: string, ...args: string[]) => {
